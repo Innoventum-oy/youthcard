45e5810508fa9deee221105e7d748825e9645549
Janne Leinonen <janlei@student.uef.fi>
2021-05-26 11:38:07 +0300

    added CZ locale file

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -867,6 +867,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'cz'),
       Locale.fromSubtags(languageCode: 'fi'),
     ];
   }
